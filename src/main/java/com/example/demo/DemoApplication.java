e235b7f90e64265b9e98bdc12cef602b9e1cc719
Sreenath <sreenathnaidu2002@gmail.com>
2025-08-08 16:01:32 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+//"C:\Users\TupiliSreenath\Apache\Maven\apache-maven-3.9.11\bin\mvn.cmd" spring-boot:run
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
